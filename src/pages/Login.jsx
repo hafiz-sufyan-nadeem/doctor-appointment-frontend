c27aa841701c179cd0baec618e83288ec1f2327d
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2026-08-15 21:55:50 +0500

    Connect Login page to Laravel API using axios, handle success/error states

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8000/api/login',{
+        email: email,
+        password: password,
+      })
+      console.log(response.data)
+    } catch (error) {
+      setError("Login failed. Check your credentials.");
+      console.logP(error)
+    }
+  }
+
   return (
-    <div>Login</div>
+    <>
+    <h2>Login</h2>
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      placeholder='Email' 
+      />
+
+      <input 
+      type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      placeholder='Password' 
+      />
+
+      <button type='submit'>Login</button>
+    </form>
+
+    </>
   )
 }
 
